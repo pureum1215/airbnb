2af789190903bd0e34e80429f383ec6733022f2e
jjh <jinjh99@gmail.com>
2025-05-14 11:44:33 +0900

    Feat: userVO 추가

---
+++ src/main/java/dummy/vo/UserVO.java
@@ -0,0 +1,13 @@
+package dummy.vo;
+
+public class UserVO {
+
+	private String userId;
+	private String userName;
+	private String userEmail;
+	private String userPassword;
+	private String phoneNumber;
+	private String userBirthday;
+	
+	
+}

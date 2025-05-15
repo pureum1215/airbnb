a6e3c5d7bff2e0354fde427ccafe98ec84628cdc
jjh <jinjh99@gmail.com>
2025-05-15 13:57:32 +0900

    사진 저장

--- src/main/java/dummy/init/PropertyInitData.java
+++ src/main/java/dummy/init/PropertyInitData.java
@@ -12,9 +12,9 @@ public class PropertyInitData implements DummyAction {
 		System.out.println("============================= property init db =============================");
 		List<PropertyVO> lists = new ArrayList<>();
 
-		lists.add(new PropertyVO("prop001", "host001", "loc001", 1, "서울 강남 아파트", "럭셔리한 강남 한복판 아파트", 120000, 2, 2, 1, "예약 가능", "http://image.com/1.jpg", "N"));
-		lists.add(new PropertyVO("prop002", "host002", "loc002", 2, "부산 해운대 뷰", "바다 보이는 오션뷰", 90000, 1, 1, 1, "예약 가능", "http://image.com/2.jpg", "N"));
-		lists.add(new PropertyVO("prop003", "host003", "loc003", 3, "제주 독채 민박", "조용한 시골의 독채", 70000, 3, 2, 2, "예약 가능", "http://image.com/3.jpg", "N"));
+		lists.add(new PropertyVO("prop001", "host001", "loc001", 1, "서울 강남 아파트", "럭셔리한 강남 한복판 아파트", 120000, 2, 2, 1, "예약 가능", "prop001.jpg", "N"));
+		lists.add(new PropertyVO("prop002", "host002", "loc002", 2, "부산 해운대 뷰", "바다 보이는 오션뷰", 90000, 1, 1, 1, "예약 가능", "prop002.jpg", "N"));
+		lists.add(new PropertyVO("prop003", "host003", "loc003", 3, "제주 독채 민박", "조용한 시골의 독채", 70000, 3, 2, 2, "예약 가능", "prop003.jpg", "N"));
 
 		for(PropertyVO p : lists) {
 			if(dummyDAO.initProperty(p))

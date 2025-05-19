70e984e6fc4693929a365a41274d3743c21cfea4
pureum1215 <pureum1215@naver.com>
2025-05-19 09:53:39 +0900

    reservationDAO 추가

---
+++ src/main/java/reservationPage/ReservationDAO.java
@@ -0,0 +1,48 @@
+package reservationPage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class ReservationDAO {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public ReservationDAO() {
+		try {
+			Context init = new InitialContext();
+			DataSource ds = (DataSource) init.lookup("java:comp/env/jdbc/MysqlDB");
+			conn = ds.getConnection();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void closeCon() {
+		try {
+			if (rs != null)
+				rs.close();
+		} catch (Exception e) {
+		}
+		try {
+			if (pstmt != null)
+				pstmt.close();
+		} catch (Exception e) {
+		}
+		try {
+			if (conn != null)
+				conn.close();
+		} catch (Exception e) {
+		}
+	}
+	
+
+
+}

654c203a4ded18b6acbf2f73d07a1a10c843a642
jjh <jinjh99@gmail.com>
2025-05-19 19:47:03 +0900

    현재까지

--- src/main/java/mainPage/mainPropertyList/MainPropertyListController.java
+++ src/main/java/mainPage/mainPropertyList/MainPropertyListController.java
@@ -1,30 +1,27 @@
-package controller;
+package mainPage.mainPropertyList;
 
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import mainPage.mainPropertyDetail.PropertyDAO;
-import mainPage.mainPropertyList.MainPropertyListDAO;
-import mainPage.mainPropertyList.MainPropertyListVO;
-
 /**
- * Servlet implementation class MainController
+ * Servlet implementation class MainPropertyListController
  */
-@WebServlet("/MainController")
-public class MainController extends HttpServlet {
+@WebServlet("/MainPropertyListController")
+public class MainPropertyListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	MainPropertyListDAO mainPropertyDAO = new MainPropertyListDAO(); // DAO 객체 준비
-	PropertyDAO propDAO = new PropertyDAO();
+	MainPropertyListDAO dao = new MainPropertyListDAO();
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
-    public MainController() {
+    public MainPropertyListController() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
@@ -33,8 +30,16 @@ public class MainController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		process(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 1. 전체 숙소 정보 가져오기
+		List<MainPropertyListVO> propertyList = dao.allgetProperty();
 
+		// 2. JSP에서 사용할 수 있게 request에 저장
+		request.setAttribute("propertyList", propertyList);
+
+		// 3. JSP로 이동
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/main_property_list.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
@@ -42,13 +47,7 @@ public class MainController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		process(request, response);
-	}
-	
-	protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		
+		doGet(request, response);
 	}
 
 }

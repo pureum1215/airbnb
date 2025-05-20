5b28f985e5a2071f69bc89cf2295615cd49aa381
pureum1215 <pureum1215@naver.com>
2025-05-20 16:27:40 +0900

    history list 출력 기능 추가

--- src/main/java/controller/api/userReservation/UserReservationHistoryAPI.java
+++ src/main/java/controller/api/userReservation/UserReservationHistoryAPI.java
@@ -1,41 +1,64 @@
 package controller.api.userReservation;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class UserReservationHistoryAPI
- */
-@WebServlet("/UserReservationHistoryAPI")
+import com.google.gson.Gson;
+
+import userPage.userReservationList.UserReservationListDAO;
+import userPage.userReservationList.UserReservationListVO;
+
+
 public class UserReservationHistoryAPI extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserReservationHistoryAPI() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doProcess(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String url = request.getRequestURI();
+		String path = request.getContextPath();
+		String command = url.substring(path.length());
+		System.out.println("api호출: " + command);
+		
+		Gson gson = new Gson();
+		
+		// 세션에서 user_id 가져오기
+        HttpSession session = request.getSession();
+        String userId = (String) session.getAttribute("userId");
+        
+        // 로그인 안 된 경우 처리
+        if (userId == null) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "로그인이 필요합니다.");
+            return;
+        }
+        
+        // DAO에서 예약 목록 가져오기
+        UserReservationListDAO dao = new UserReservationListDAO();
+        List<UserReservationListVO> historyList = dao.getHistoryReservations(userId);
+
+		
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json; charset=UTF-8");
+
+		PrintWriter out = response.getWriter();
+		out.print(gson.toJson(historyList));
+		out.flush();
+
 	}
 
 }
--- src/main/java/userPage/userReservationList/UserReservationListDAO.java
+++ src/main/java/userPage/userReservationList/UserReservationListDAO.java
@@ -78,5 +78,37 @@ public class UserReservationListDAO {
 
 	    return list;
 	} 
+	
+	public List<UserReservationListVO> getHistoryReservations(String userId) {
+	    List<UserReservationListVO> list = new ArrayList<>();
+	    try {
+	    	String sql = "SELECT r.reservation_id, p.property_name, p.property_photo_url,"
+        		+ "l.location_city, l.location_country, r.reservation_check_in, r.reservation_check_out"
+        		+ "FROM reservation"
+        		+ "JOIN property p ON r.property_id = p.property_id"
+        		+ "JOIN location l ON p.location_id = l.location_id"
+        		+ "WHERE r.user_id = ?"
+        		+ "ORDER BY r.reservation_check_in ASC;";
+	        pstmt = conn.prepareStatement(sql);
+	        pstmt.setString(1, userId);
+	        rs = pstmt.executeQuery();
+
+	        while (rs.next()) {
+	        	UserReservationListVO vo = new UserReservationListVO();
+	        	vo.setProperty_id(rs.getString("Property_id"));
+	        	vo.setReservation_check_in(rs.getDate("check_in"));
+	        	vo.setReservation_check_out(rs.getDate("check_out"));
+	            list.add(vo);
+	        }
+	    } 
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    } 
+	    finally {
+	        closeCon();
+	    }
+
+	    return list;
+	}
 
 }

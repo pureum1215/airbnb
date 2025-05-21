0eca7b5b051ed5c9b003c3a649deacf5bb600034
pureum1215 <pureum1215@naver.com>
2025-05-21 10:28:05 +0900

    reservation 부분 db 연결 문제 해결

--- src/main/java/reservationPage/reservationPayment/ReservationPaymentAction.java
+++ src/main/java/reservationPage/reservationPayment/ReservationPaymentAction.java
@@ -1,5 +1,8 @@
 package reservationPage.reservationPayment;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,26 +23,44 @@ public class ReservationPaymentAction implements Action {
 		String reservationId = request.getParameter("resesrvation_id");
 		String propertyId = request.getParameter("property_id");
 		
-		
-		// payment_id 값 정하기
 		ReservationDAO dao = new ReservationDAO();
-		String lastPaymentId = dao.getLastPaymentId();
-		int number = Integer.parseInt(lastPaymentId.substring(3));
-		String newPaymentId = String.format("res%03d", number + 1);
+
 		
-		// 총 비용 구하기
-		int totalPrice = dao.getTotalPrice(propertyId);
+		try {
+			// payment_id 값 정하기
+			String lastPaymentId = dao.getLastPaymentId();
+			int number = Integer.parseInt(lastPaymentId.substring(3));
+			String newPaymentId = String.format("res%03d", number + 1);
+			
+			// 총 비용 구하기
+			int totalPrice = dao.getTotalPrice(propertyId);
+			
+			
+			// vo에 값 세팅
+			ReservationPaymentVO vo = new ReservationPaymentVO();
+			vo.setPayment_id(newPaymentId);
+			vo.setReservation_id(reservationId);
+			vo.setPayment_price(totalPrice);
+			vo.setPayment_method(paymentMethod);
+			
+			// 결제 기능
+			dao.reservationPayment(vo);
+		}
+		catch(SQLException e) {
+			System.out.println("sql");
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			System.out.println("input, output");
+			e.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			dao.closeCon();
+		}
 		
-		
-		// vo에 값 세팅
-		ReservationPaymentVO vo = new ReservationPaymentVO();
-		vo.setPayment_id(newPaymentId);
-		vo.setReservation_id(reservationId);
-		vo.setPayment_price(totalPrice);
-		vo.setPayment_method(paymentMethod);
-		
-		// 결제 기능
-		dao.reservationPayment(vo);
 		
         ActionForward forward = new ActionForward();
         forward.setPath("user/userReservationList.jsp");

fb68bd77f652e00ec071dbe5b109038ea237a3fd
pureum1215 <pureum1215@naver.com>
2025-05-26 18:21:15 +0900

    main property list 출력 method 껍데기만 작성

--- src/main/java/mainPage/mainPropertyList/MainPropertyListDAO.java
+++ src/main/java/mainPage/mainPropertyList/MainPropertyListDAO.java
@@ -46,76 +46,6 @@ public class MainPropertyListDAO {
 		}
 	}
 	
-	public List<MainPropertyListSearchVO> searchProperties(MainPropertyListSearchVO criteria) {
-	    List<MainPropertyListSearchVO> list = new ArrayList<>();
-
-	    try {
-	        StringBuilder sql = new StringBuilder(
-	            "SELECT * FROM main_property_list_view WHERE 1=1 "
-	        );
-
-	        List<Object> paramList = new ArrayList<>();
-
-	        // 대륙, 나라, 도시
-	        if (criteria.getLocation_continent() != null && !criteria.getLocation_continent().isEmpty()) {
-	            sql.append("AND location_continent = ? ");
-	            paramList.add(criteria.getLocation_continent());
-	        }
-
-	        if (criteria.getLocation_country() != null && !criteria.getLocation_country().isEmpty()) {
-	            sql.append("AND location_country = ? ");
-	            paramList.add(criteria.getLocation_country());
-	        }
-
-	        if (criteria.getLocation_city() != null && !criteria.getLocation_city().isEmpty()) {
-	            sql.append("AND location_city = ? ");
-	            paramList.add(criteria.getLocation_city());
-	        }
-
-	        // 예약 날짜 범위
-	        if (criteria.getReservation_check_in() != null && criteria.getReservation_check_out() != null) {
-	            sql.append("AND property_id NOT IN ( ")
-	               .append("SELECT property_id FROM reservation ")
-	               .append("WHERE NOT (reservation_check_out <= ? OR reservation_check_in >= ?) ) ");
-	            paramList.add(criteria.getReservation_check_in());
-	            paramList.add(criteria.getReservation_check_out());
-	        }
-
-	        // 가격
-	        
-
-	        // 편의시설
-	       
-
-	        pstmt = conn.prepareStatement(sql.toString());
-
-	        for (int i = 0; i < paramList.size(); i++) {
-	            pstmt.setObject(i + 1, paramList.get(i));
-	        }
-
-	        rs = pstmt.executeQuery();
-
-	        while (rs.next()) {
-	        	MainPropertyListSearchVO vo = new MainPropertyListSearchVO();
-	            vo.setProperty_id(rs.getString("property_id"));
-	            vo.setProperty_photo_url(rs.getString("property_photo_url"));
-	            vo.setPrice_per_night(rs.getInt("price_per_night"));
-	            vo.setProperty_description(rs.getString("property_description"));
-	            vo.setProperty_review_rating(rs.getInt("property_review_rating"));
-	            vo.setLocation_city(rs.getString("location_city"));
-	            vo.setLocation_country(rs.getString("location_country"));
-	            // 기타 필요 데이터 추가
-	            list.add(vo);
-	        }
-
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    } finally {
-	        closeCon();
-	    }
-
-	    return list;
-	}
 
 	public List<MainPropertyListVO> allgetProperty() {
 		List<MainPropertyListVO> lists = new ArrayList<MainPropertyListVO>();
@@ -141,4 +71,29 @@ public class MainPropertyListDAO {
 		System.out.println("check-d: "+lists.size());
 		return lists;
 	}
+	
+	
+	
+	public List<MainPropertyListVO> getPropertyList( List<String> property_id_list, String user_id ) {
+		System.out.println("getAllPropertyId method 호출");
+		
+		List<MainPropertyListVO> list = new ArrayList<>();
+		
+
+		
+		return list;
+	}
+	
+	public List<MainPropertyListVO> getAllPropertyList() {
+		System.out.println("getAllPropertyId method 호출");
+		
+		List<MainPropertyListVO> list = new ArrayList<>();
+		
+		return list;
+	}
+		
+	
+	
+	
+	
 }
--- src/main/java/mainPage/mainPropertyList/MainPropertyListVO.java
+++ src/main/java/mainPage/mainPropertyList/MainPropertyListVO.java
@@ -1,56 +1,41 @@
 package mainPage.mainPropertyList;
 
-import java.sql.Date;
-
 public class MainPropertyListVO {
 	
 /************************************************ 
- * 숙소 위치 id, 나라, 도시, 대륙, 어메니티 종류, 
- * 체크인 날짜, 체크아웃 날짜, 숙소 id, 숙소 삭제 여부
- * 
- * 숙소 사진, 별점, 숙소 설명, 1박 가격,
- * 사용자 id, 사용자 이름, 사용자 삭제 여부
+ * 숙소 id, 숙소 사진, 숙소 이름, 나라, 도시, 
+ * 숙소 설명, 1박 가격, 별점 평균, 위시리스트, 삭제 여부,
 **************************************************/
 	
-	private String location_id;
-	private String location_continent;
+	private String property_id;
+	private String property_name;
+	private String property_photo_url;
 	private String location_country;
 	private String location_city;
-	private int amenity_id;
-	private Date reservation_check_in;
-	private Date reservation_check_out;
-	private String property_id;
-	private String property_delete_yn;
-	
-	private String property_name; //숙소 이름
-	private String property_photo_url;
-	private int property_review_rating;
+
 	private String property_description;
 	private int price_per_night;
+	private double property_review_rating;
+	private String wish_list;
+	private String property_delete_yn;
 	
-	private String user_id;
-	private String user_name;
-	private String user_delete_yn;
-	
-	
-	
+	public String getProperty_id() {
+		return property_id;
+	}
+	public void setProperty_id(String property_id) {
+		this.property_id = property_id;
+	}
 	public String getProperty_name() {
 		return property_name;
 	}
 	public void setProperty_name(String property_name) {
 		this.property_name = property_name;
 	}
-	public String getLocation_id() {
-		return location_id;
+	public String getProperty_photo_url() {
+		return property_photo_url;
 	}
-	public void setLocation_id(String location_id) {
-		this.location_id = location_id;
-	}
-	public String getLocation_continent() {
-		return location_continent;
-	}
-	public void setLocation_continent(String location_continent) {
-		this.location_continent = location_continent;
+	public void setProperty_photo_url(String property_photo_url) {
+		this.property_photo_url = property_photo_url;
 	}
 	public String getLocation_country() {
 		return location_country;
@@ -64,48 +49,6 @@ public class MainPropertyListVO {
 	public void setLocation_city(String location_city) {
 		this.location_city = location_city;
 	}
-	public int getAmenity_id() {
-		return amenity_id;
-	}
-	public void setAmenity_id(int amenity_id) {
-		this.amenity_id = amenity_id;
-	}
-	public Date getReservation_check_in() {
-		return reservation_check_in;
-	}
-	public void setReservation_check_in(Date reservation_check_in) {
-		this.reservation_check_in = reservation_check_in;
-	}
-	public Date getReservation_check_out() {
-		return reservation_check_out;
-	}
-	public void setReservation_check_out(Date reservation_check_out) {
-		this.reservation_check_out = reservation_check_out;
-	}
-	public String getProperty_id() {
-		return property_id;
-	}
-	public void setProperty_id(String property_id) {
-		this.property_id = property_id;
-	}
-	public String getProperty_delete_yn() {
-		return property_delete_yn;
-	}
-	public void setProperty_delete_yn(String property_delete_yn) {
-		this.property_delete_yn = property_delete_yn;
-	}
-	public String getProperty_photo_url() {
-		return property_photo_url;
-	}
-	public void setProperty_photo_url(String property_photo_url) {
-		this.property_photo_url = property_photo_url;
-	}
-	public int getProperty_review_rating() {
-		return property_review_rating;
-	}
-	public void setProperty_review_rating(int property_review_rating) {
-		this.property_review_rating = property_review_rating;
-	}
 	public String getProperty_description() {
 		return property_description;
 	}
@@ -118,40 +61,23 @@ public class MainPropertyListVO {
 	public void setPrice_per_night(int price_per_night) {
 		this.price_per_night = price_per_night;
 	}
-	public String getUser_id() {
-		return user_id;
+	public double getProperty_review_rating() {
+		return property_review_rating;
 	}
-	public void setUser_id(String user_id) {
-		this.user_id = user_id;
+	public void setProperty_review_rating(double property_review_rating) {
+		this.property_review_rating = property_review_rating;
 	}
-	public String getUser_name() {
-		return user_name;
+	public String getWish_list() {
+		return wish_list;
 	}
-	public void setUser_name(String user_name) {
-		this.user_name = user_name;
+	public void setWish_list(String wish_list) {
+		this.wish_list = wish_list;
 	}
-	public String getUser_delete_yn() {
-		return user_delete_yn;
+	public String getProperty_delete_yn() {
+		return property_delete_yn;
 	}
-	public void setUser_delete_yn(String user_delete_yn) {
-		this.user_delete_yn = user_delete_yn;
+	public void setProperty_delete_yn(String property_delete_yn) {
+		this.property_delete_yn = property_delete_yn;
 	}
-	
-	
-	@Override
-	public String toString() {
-		return "MainPropertyListVO [location_id=" + location_id + ", location_continent=" + location_continent
-				+ ", location_country=" + location_country + ", location_city=" + location_city + ", amenity_id="
-				+ amenity_id + ", reservation_check_in=" + reservation_check_in + ", reservation_check_out="
-				+ reservation_check_out + ", property_id=" + property_id + ", property_delete_yn=" + property_delete_yn
-				+ ", property_name=" + property_name + ", property_photo_url=" + property_photo_url
-				+ ", property_review_rating=" + property_review_rating + ", property_description="
-				+ property_description + ", price_per_night=" + price_per_night + ", user_id=" + user_id
-				+ ", user_name=" + user_name + ", user_delete_yn=" + user_delete_yn + "]";
-	}
-	
-
-	
-	
 
 }

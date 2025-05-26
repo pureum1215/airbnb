1594ec008b0d260498a3de797435fb8d090b5611
pureum1215 <pureum1215@naver.com>
2025-05-26 15:39:29 +0900

    가격, 위치, 방 수 필터 적용

--- src/main/java/mainPage/mainPropertyList/MainPropertyListSearchDAO.java
+++ src/main/java/mainPage/mainPropertyList/MainPropertyListSearchDAO.java
@@ -1,5 +1,239 @@
 package mainPage.mainPropertyList;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class MainPropertyListSearchDAO {
 
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public MainPropertyListSearchDAO() {
+		System.out.println("생성 체크");
+		
+		try {
+			Context init = new InitialContext();
+			DataSource ds = (DataSource) init.lookup("java:comp/env/jdbc/MysqlDB");
+			conn = ds.getConnection();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void closeCon() {
+		try {
+			if (rs != null)
+				rs.close();
+		} catch (Exception e) {
+		}
+		try {
+			if (pstmt != null)
+				pstmt.close();
+		} catch (Exception e) {
+		}
+		try {
+			if (conn != null)
+				conn.close();
+		} catch (Exception e) {
+		}
+	}
+	
+	
+	// 모든 property_id list에 담기
+	public List<String> getAllPropertyId() throws SQLException, IOException {
+		System.out.println("getAllPropertyId method 호출");
+		
+		List<String> list = new ArrayList<>();
+		
+		String sql = "SELECT property_id FROM PROPERTY";
+		
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+		
+        while (rs.next()) {
+        	String propertyId = rs.getString("property_id");
+            list.add(propertyId);
+        }
+		return list;
+	}
+	
+	
+	
+	// 가격 필터
+	public List<String> filterByPrice(int min_price, int max_price) throws SQLException, IOException {
+		System.out.println("filterByPrice method 호출");
+		
+		List<String> list = new ArrayList<>();
+		
+		String sql = "SELECT property_id "
+				+ "FROM property "
+				+ "WHERE price_per_night BETWEEN ? AND ? ";
+		
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+		
+        while (rs.next()) {
+        	String propertyId = rs.getString("property_id");
+        	pstmt.setInt(1, min_price);
+        	pstmt.setInt(1, max_price);
+            list.add(propertyId);
+        }
+		return list;
+	}
+	
+	// 대륙 필터
+	public List<String> filterByContinent(String continent) throws SQLException, IOException {
+		System.out.println("filterByContinent method 호출");
+		
+		List<String> list = new ArrayList<>();
+		
+		String sql = "SELECT p.property_id "
+				+ "FROM property p "
+				+ "JOIN location l ON p.location_id = l.location_id "
+				+ "WHERE l.location_countinent = ? ";
+		
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+		
+        while (rs.next()) {
+        	String propertyId = rs.getString("property_id");
+        	pstmt.setString(1, continent);
+            list.add(propertyId);
+        }
+		return list;
+	}
+	
+	// 나라 필터
+	public List<String> filterByCountry(String country) throws SQLException, IOException {
+		System.out.println("filterByCountry method 호출");
+		
+		List<String> list = new ArrayList<>();
+		
+		String sql = "SELECT p.property_id "
+				+ "FROM property p "
+				+ "JOIN location l ON p.location_id = l.location_id "
+				+ "WHERE l.location_country = ? ";
+		
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+		
+        while (rs.next()) {
+        	String propertyId = rs.getString("property_id");
+        	pstmt.setString(1, country);
+            list.add(propertyId);
+        }
+		return list;
+	}
+	
+	// 도시 필터
+	public List<String> filterByCity(String city) throws SQLException, IOException {
+		System.out.println("filterByCity method 호출");
+		
+		List<String> list = new ArrayList<>();
+		
+		String sql = "SELECT p.property_id "
+				+ "FROM property p "
+				+ "JOIN location l ON p.location_id = l.location_id "
+				+ "WHERE l.location_city = ? ";
+		
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+		
+        while (rs.next()) {
+        	String propertyId = rs.getString("property_id");
+        	pstmt.setString(1, city);
+            list.add(propertyId);
+        }
+		return list;
+	}
+	
+	// 침실 수 필터
+	public List<String> filterByRoom(int room) throws SQLException, IOException {
+		System.out.println("filterByRoom method 호출");
+		
+		List<String> list = new ArrayList<>();
+		
+		String sql = "SELECT property_id "
+				+ "FROM property "
+				+ "WHERE property_room >= ? ";
+		
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+		
+        while (rs.next()) {
+        	String propertyId = rs.getString("property_id");
+        	pstmt.setInt(1, room);
+            list.add(propertyId);
+        }
+		return list;
+	}
+	
+	// 침대 수 필터
+	public List<String> filterByBed(int bed) throws SQLException, IOException {
+		System.out.println("filterByBed method 호출");
+		
+		List<String> list = new ArrayList<>();
+		
+		String sql = "SELECT property_id "
+				+ "FROM property "
+				+ "WHERE property_bed >= ? ";
+		
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+		
+        while (rs.next()) {
+        	String propertyId = rs.getString("property_id");
+        	pstmt.setInt(1, bed);
+            list.add(propertyId);
+        }
+		return list;
+	}
+	
+	// 욕실 수 필터
+	public List<String> filterByBath(int bath) throws SQLException, IOException {
+		System.out.println("filterByBath method 호출");
+		
+		List<String> list = new ArrayList<>();
+		
+		String sql = "SELECT property_id "
+				+ "FROM property "
+				+ "WHERE property_bath >= ? ";
+		
+        pstmt = conn.prepareStatement(sql);
+        rs = pstmt.executeQuery();
+		
+        while (rs.next()) {
+        	String propertyId = rs.getString("property_id");
+        	pstmt.setInt(1, bath);
+            list.add(propertyId);
+        }
+		return list;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/mainPage/mainPropertyList/MainPropertySearchAction.java
+++ src/main/java/mainPage/mainPropertyList/MainPropertySearchAction.java
@@ -1,5 +1,8 @@
 package mainPage.mainPropertyList;
 
+import java.sql.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -18,8 +21,78 @@ public class MainPropertySearchAction implements Action {
         response.setCharacterEncoding("UTF-8");
 		
         
+        // 대륙, 나라, 도시, 날짜, 가격, 침실/침대/욕실 수, 편의시설
+        String continent = request.getParameter("location_continent");
+        String country = request.getParameter("location_country");
+        String city = request.getParameter("location_city");
+        Date check_in = Date.valueOf(request.getParameter("reservation_check_in"));
+        Date check_out = Date.valueOf(request.getParameter("reservation_check_out"));
+        int min_price = Integer.parseInt(request.getParameter("min_price_per_night"));
+        int max_price = Integer.parseInt(request.getParameter("max_price_per_night"));
+        int room = Integer.parseInt(request.getParameter("property_room"));
+        int bed = Integer.parseInt(request.getParameter("property_bed"));
+        int bath = Integer.parseInt(request.getParameter("property_bath"));
+        // 편의시설
+        
+        MainPropertyListSearchDAO dao = new MainPropertyListSearchDAO();
+        
+        // 필터로 한 번 걸러진 id와 새로운 필터에서 걸러진 내용을 비교, 새로운 filter_list에 id 담기
+        // filter_list에 담으면 list 내용을 filter_list 내용으로 덮어쓰기
+        // 반복
+        
+        List<String> property_id_list = null;
+        List<String> property_id_filter_list = null;
+
+        // 모든 id 다 가져오기
+        property_id_list = dao.getAllPropertyId();
+        property_id_filter_list = dao.getAllPropertyId();
+        
+        // 가격 필터
+        	property_id_filter_list = dao.filterByPrice(min_price, max_price);
+        	property_id_list.retainAll(property_id_filter_list);
+
+        // 대륙 필터
+        if ( continent != null ) {
+        	property_id_filter_list = dao.filterByContinent(continent);
+        	property_id_list.retainAll(property_id_filter_list);
+        }
+        // 나라 필터
+        if ( country != null ) {
+        	property_id_filter_list = dao.filterByCountry(country);
+        	property_id_list.retainAll(property_id_filter_list);
+        }
+        // 도시 필터
+        if ( city != null ) {
+        	property_id_filter_list = dao.filterByCity(city);
+        	property_id_list.retainAll(property_id_filter_list);
+        }
+        // 날짜 필터
+//        if (  != null ) {
+//        	property_id_filter_list = dao.filterBy();
+//        	property_id_list.retainAll(property_id_filter_list);
+//        }
+        // 침실 수 필터
+        if ( room != 0 ) {
+        	property_id_filter_list = dao.filterByRoom(room);
+        	property_id_list.retainAll(property_id_filter_list);
+        }
+        // 침대 수 필터
+        if ( bed != 0 ) {
+        	property_id_filter_list = dao.filterByBed(bed);
+        	property_id_list.retainAll(property_id_filter_list);
+        }
+        // 욕실 수 필터
+        if ( bath != 0 ) {
+        	property_id_filter_list = dao.filterByBath(bath);
+        	property_id_list.retainAll(property_id_filter_list);
+        }
+        // 편의시설 필터
+        if ( continent != null ) {
+        	
+        }
         
         
+    
         
         
         
